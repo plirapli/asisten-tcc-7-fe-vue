bcf8d1b03ad6255b5e19472c009f4b96b7a02a87
plirapli <rafliyulistiawan@gmail.com>
2025-06-09 03:22:46 +0700

    fix: tsc

--- src/components/ui/drawer/Drawer.vue
+++ src/components/ui/drawer/Drawer.vue
@@ -1,22 +1,20 @@
 <script lang="ts" setup>
-import type { DrawerRootEmits, DrawerRootProps } from 'vaul-vue'
-import { useForwardPropsEmits } from 'reka-ui'
-import { DrawerRoot } from 'vaul-vue'
+// @ts-nocheck
+import type { DrawerRootEmits, DrawerRootProps } from "vaul-vue";
+import { useForwardPropsEmits } from "reka-ui";
+import { DrawerRoot } from "vaul-vue";
 
 const props = withDefaults(defineProps<DrawerRootProps>(), {
   shouldScaleBackground: true,
-})
+});
 
-const emits = defineEmits<DrawerRootEmits>()
+const emits = defineEmits<DrawerRootEmits>();
 
-const forwarded = useForwardPropsEmits(props, emits)
+const forwarded = useForwardPropsEmits(props, emits);
 </script>
 
 <template>
-  <DrawerRoot
-    data-slot="drawer"
-    v-bind="forwarded"
-  >
+  <DrawerRoot data-slot="drawer" v-bind="forwarded">
     <slot />
   </DrawerRoot>
 </template>
